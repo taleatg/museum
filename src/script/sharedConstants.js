149dc48d05f044eaedaf706122e7a5bfe64d910f
taleatg <t_korbyt@mail.ru>
2022-05-26 20:04:11 +0300

    feat: add project

--- src/script/sharedConstants.js
+++
@@ -1,9 +0,0 @@
-const musicPlay = document.querySelector('.music-play');
-const audio = document.querySelector('#audio');
-const video = document.querySelector('video');
-
-export {
-    musicPlay,
-    audio,
-    video,
-}
